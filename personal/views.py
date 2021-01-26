f5d4c24562dea9daa15246b2ddd6a350712660a3
hashim <hashimtahir@live.com>
2021-01-26 11:11:16 +0500

    Pass data (variable / lists) to a template

--- personal/views.py
+++ personal/views.py
@@ -4,5 +4,26 @@ from django.shortcuts import render
 
 
 def home_screen_view(request):
-    print(request.body)
-    return render(request, "personal/home.html", {})
+    context = {}
+    # Oneway
+    # context['data_string'] = "String that is to be passed to the view."
+    # context['data_number'] = 125663
+
+    # Other way
+
+    # context = {
+    #     'data_string': "String that is to be passed to the view.",
+    #     'data_number': 125663
+    # }
+
+    # Passs list of data
+
+    list_of_views = []
+    list_of_views.append("a")
+    list_of_views.append("b")
+    list_of_views.append("c")
+    list_of_views.append("ds")
+
+    context['list_name_dosent_matter_here'] = list_of_views
+
+    return render(request, "personal/home.html", context)
