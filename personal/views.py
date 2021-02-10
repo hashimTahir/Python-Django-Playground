9bac91ce9f5d7138b49074d55bf7a93d31253393
hashim <hashimtahir@live.com>
2021-02-11 00:03:31 +0500

    Blog feed snippet added to the home page.Create, edit blog all working.

--- personal/views.py
+++ personal/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from account.models import Account
+from blog.models import BlogPost
+from operator import attrgetter
 
 # Create your views here.
 
 
 def home_screen_view(request):
     context = {}
-    accounts = Account.objects.all()
-    context['accounts'] = accounts
+
+    # Sort the blog post by date updated
+    blog_posts = sorted(BlogPost.objects.all(),
+                        key=attrgetter('date_updated'), reverse=True)
+
+    context['blog_posts'] = blog_posts
 
     return render(request, "personal/home.html", context)
