0535fb3bf5fee2cb3787ff31189911099dd8d9cd
hashim <hashimtahir@live.com>
2021-02-10 23:32:22 +0500

    Blog edit wokring.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from blog.views import (
-    create_blog_view,
-    detail_blog_view,
+	create_blog_view,
+	detail_blog_view,
+	edit_blog_view,
 )
 
-# whenever an app is created which has its own urls, app_name
-# parameter must be added. apps urls, required by django
 app_name = 'blog'
 
 urlpatterns = [
-    path('create', create_blog_view, name="create"),
+    path('create/', create_blog_view, name="create"),
     path('<slug>/', detail_blog_view, name="detail"),
-]
+    path('<slug>/edit/', edit_blog_view, name="edit"),
+ ]
+
