31bad1b471c679bec183007f33f4385e134df82e
hashim <hashimtahir@live.com>
2021-02-13 00:20:15 +0500

    Create, update,delete apis created. Wokring with postman.

--- blog/api/views.py
+++ blog/api/views.py
@@ -7,6 +7,13 @@ from blog.models import BlogPost
 from blog.api.serializers import BlogPostSerializer
 
 
+SUCCESS = 'success'
+ERROR = 'error'
+DELETE_SUCCESS = 'deleted'
+UPDATE_SUCCESS = 'updated'
+CREATE_SUCCESS = 'created'
+
+
 @api_view(['GET', ])
 def api_detail_blog_view(request, slug):
 
@@ -18,3 +25,54 @@ def api_detail_blog_view(request, slug):
     if request.method == 'GET':
         serializer = BlogPostSerializer(blog_post)
         return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+def api_update_blog_view(request, slug):
+
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = BlogPostSerializer(blog_post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data[SUCCESS] = UPDATE_SUCCESS
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+def api_delete_blog_view(request, slug):
+
+    try:
+        blog_post = BlogPost.objects.get(slug=slug)
+    except BlogPost.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        operation = blog_post.delete()
+        data = {}
+        if operation:
+            data[SUCCESS] = DELETE_SUCCESS
+        return Response(data=data)
+
+
+@api_view(['POST'])
+def api_create_blog_view(request):
+
+    # For testing only pass the static account later to be replaced by auth token
+    account = Account.objects.get(pk=1)
+
+    blog_post = BlogPost(author=account)
+
+    if request.method == 'POST':
+        serializer = BlogPostSerializer(blog_post, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
