da32febee5af35fcd4284aa150f3bcdf7932a5e2
hashim <hashimtahir@live.com>
2021-02-16 23:32:18 +0500

    Serializer method fields to retrieve the author of blogpost in api response.

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -4,10 +4,18 @@ from blog.models import BlogPost
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
+
+# as mentioned in docs. get_username_from_author is the method name
+    username = serializers.SerializerMethodField('get_username_from_author')
+
     class Meta:
         model = BlogPost
-        fields = ['title', 'body', 'image', 'date_updated']
+        fields = ['title', 'body', 'image', 'date_updated', 'username']
 
+
+    def get_username_from_author(self,blog_post):
+        username =blog_post.author.username
+        return username
     # https://www.django-rest-framework.org/
     # There are two type of serilizers. General and model.
     # general serilizers are much more customizable.
