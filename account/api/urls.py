d2787c32c8df1e584c4c921d0241d68da021f8d1
hashim <hashimtahir@live.com>
2021-02-18 00:29:07 +0500

    Create Auth Token when user logs in, if token dosent exists.

--- account/api/urls.py
+++ account/api/urls.py
@@ -3,6 +3,7 @@ from account.api.views import(
     registration_view,
     account_properties_view,
     update_account_view,
+    ObtainAuthTokenView
 
 )
 from rest_framework.authtoken.views import obtain_auth_token
@@ -11,6 +12,6 @@ app_name = 'account'
 urlpatterns = [
     path('properties', account_properties_view, name="properties"),
     path('properties/update', update_account_view, name="update"),
-    path('login', obtain_auth_token, name="login"),
+    path('login', ObtainAuthTokenView.as_view(), name="login"),
     path('register', registration_view, name="register"),
 ]
--- account/api/views.py
+++ account/api/views.py
@@ -5,7 +5,8 @@ from rest_framework.authtoken.models import Token
 from account.api.serializers import RegistrationSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
 from account.api.serializers import RegistrationSerializer, AccountPropertiesSerializer
 from account.models import Account
 from rest_framework.authtoken.models import Token
@@ -69,3 +70,28 @@ def update_account_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ObtainAuthTokenView(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        context = {}
+
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        account = authenticate(email=email, password=password)
+        if account:
+            try:
+                token = Token.objects.get(user=account)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=account)
+            context['response'] = 'Successfully authenticated.'
+            context['pk'] = account.pk
+            context['email'] = email
+            context['token'] = token.key
+        else:
+            context['response'] = 'Error'
+            context['error_message'] = 'Invalid credentials'
+
+        return Response(context)
