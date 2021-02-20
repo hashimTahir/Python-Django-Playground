c4f0c0cd27b31843807ed3967a69fbe72423243d
hashim <hashimtahir@live.com>
2021-02-20 23:25:52 +0500

    Registration api view fixed. Cant register if a user already exits.

--- account/api/views.py
+++ account/api/views.py
@@ -12,23 +12,61 @@ from account.models import Account
 from rest_framework.authtoken.models import Token
 
 
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+# Register
+# Url: https://<your-domain>/api/account/register
 @api_view(['POST', ])
+@permission_classes([])
+@authentication_classes([])
 def registration_view(request):
 
-    if request.method == 'POST':
-        serializer = RegistrationSerializer(data=request.data)
-        data = {}
-        if serializer.is_valid():
-            account = serializer.save()
-            data['response'] = 'successfully registered new user.'
-            data['email'] = account.email
-            data['username'] = account.username
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+	if request.method == 'POST':
+		data = {}
+		email = request.data.get('email', '0')
+		if validate_email(email) != None:
+			data['error_message'] = 'That email is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
 
-        else:
-            data = serializer.errors
-        return Response(data)
+		username = request.data.get('username', '0')
+		if validate_username(username) != None:
+			data['error_message'] = 'That username is already in use.'
+			data['response'] = 'Error'
+			return Response(data)
+
+		serializer = RegistrationSerializer(data=request.data)
+		
+		if serializer.is_valid():
+			account = serializer.save()
+			data['response'] = 'successfully registered new user.'
+			data['email'] = account.email
+			data['username'] = account.username
+			data['pk'] = account.pk
+			token = Token.objects.get(user=account).key
+			data['token'] = token
+		else:
+			data = serializer.errors
+		return Response(data)
+
+def validate_email(email):
+	account = None
+	try:
+		account = Account.objects.get(email=email)
+	except Account.DoesNotExist:
+		return None
+	if account != None:
+		return email
+
+def validate_username(username):
+	account = None
+	try:
+		account = Account.objects.get(username=username)
+	except Account.DoesNotExist:
+		return None
+	if account != None:
+		return username
+  
 
 
 # Account properties
